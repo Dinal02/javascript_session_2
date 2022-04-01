1456a12b0387c1d2bef11a9c2a6eddf252d52372
Dinal02 <99264322+Dinal02@users.noreply.github.com>
2022-04-01 21:14:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//Create a variable "Skill Safari" and perform the following tasks.
+//1. Find the length of the string.
+//2. Convert all the letters in the string into capital letters.
+//3. What is index position of "a"
+
+var str = "Skill Safari"
+console.log(str.length)
+console.log(str.toUpperCase())
+console.log(str.indexOf("a"))
+
+//Create a single console.log that will display a famous conversation between Captain America and Iron Man in Avengers. All the exchange should be in different lines.
+console.log("Captain America: \"We have orders, we should follow them.\" \n Iron Man: \"Following's not really my style.\" \n Captain America: \"And You are all about style, Aren't you ?\" \n Iron Man: \"Of the people in this room, which one is: A, wearing a spangly outfit; and B, not of use ? \"")
+
+//Create a Body Mass Index logic by using Height and Weight as inputs. 
+var w = 60
+var h = 170
+
+const bmi = (w) * 10000 / (h*h)
+
+console.log(bmi)
